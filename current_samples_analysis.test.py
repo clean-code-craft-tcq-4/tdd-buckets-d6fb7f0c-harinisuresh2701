1b6b2de01661df94eade060976a2f7d3b51c7e31
harinisuresh2701 <105203673+harinisuresh2701@users.noreply.github.com>
2022-08-14 16:46:07 +0530

    Update current_samples_analysis.test.py

--- current_samples_analysis.test.py
+++ current_samples_analysis.test.py
@@ -2,7 +2,10 @@ import unittest
 import current_samples_analysis
 
 class TypewiseTest(unittest.TestCase):
-  sample_set = [4,5]
+  sample_set = [3, 3, 5, 4, 10, 11, 12]
+  def check_find_range_and_count(self,sample_set):
+    self.assertTrue(current_samples_analysis.find_range_and_count(sample_set)=={(10, 12, 3), (3, 5, 4)})
+  
   def check_current_samples(self):
     count_in_range = current_samples_analysis.occurance_in_range(sample_set)
     self.assertTrue(count_in_range == 2)
