939a9b62864b63c2f8c827f1e0cb73c9437121e6
harinisuresh2701 <105203673+harinisuresh2701@users.noreply.github.com>
2022-08-25 11:37:51 +0530

    Update current_samples_analysis.test.py

--- current_samples_analysis.test.py
+++ current_samples_analysis.test.py
@@ -3,8 +3,8 @@ import current_samples_analysis
 
 class TypewiseTest(unittest.TestCase):
   sample_set = [3, 3, 5, 4, 10, 11, 12]
-  def check_find_range_and_count(self,sample_set):
-    self.assertTrue(current_samples_analysis.find_range_and_count(sample_set)=={(10, 12, 3), (3, 5, 4)})
+  def check_find_range_count(self,sample_set):
+    self.assertTrue(current_samples_analysis.find_range_count(sample_set)=={(10, 12, 3), (3, 5, 4)})
   
-  def check_print_in_csv_format(self):
+  def check_csv_formatter(self):
     self.asserTrue(current_samples_analysis.print_in_csv_format([3,5,7])=="3-5, 7")
