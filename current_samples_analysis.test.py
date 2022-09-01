931864df9972be2b467e7a6666d55b9322819002
harinisuresh2701 <105203673+harinisuresh2701@users.noreply.github.com>
2022-09-01 10:56:44 +0530

    Update current_samples_analysis.test.py

--- current_samples_analysis.test.py
+++ current_samples_analysis.test.py
@@ -7,4 +7,16 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(current_samples_analysis.find_range_count(sample_set)=={(10, 12, 3), (3, 5, 4)})
   
   def check_csv_formatter(self):
-    self.asserTrue(current_samples_analysis.print_in_csv_format([3,5,7])=="3-5, 7")
+    self.assertTrue(current_samples_analysis.print_in_csv_format([3,5,7])=="3-5, 7")
+
+  def check_error_handling(self):
+    self.assertTrue(current_sensing.error_handling(4095)=="Current sample is out of valid range of [0 - 4094]!")
+    self.assertTrue(current_sensing.error_handling(4093)=="All is well!")
+    self.assertTrue(current_sensing.error_handling(40.3)=="Current sample is not a positive integer!")
+    self.assertTrue(current_sensing.error_handling(-5)=="Current sample is not a positive integer!")
+    self.assertTrue(current_sensing.error_handling(0)=="Warning : No current at all!")
+    
+  def check_sample_to_amp_conversion(self):
+    self.assertTrue(current_sensing.sample_to_amp_conversion(0)==0)
+    self.assertTrue(current_sensing.sample_to_amp_conversion(1146)==3)
+    
